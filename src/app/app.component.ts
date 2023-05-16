f304fc30b90d60e8c9de9dc01192879eb1b246c7
morad zakar <morad.azer.1999@gmail.com>
2023-05-16 10:39:02 +0100

    second commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,19 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  standalone: true,
+  imports: [],
+  template: `<h1>Default</h1>`,
+  styleUrls: ['./app.component.css'],
+})
+export class AppComponent {
+  title = 'default';
+}
+
+
+/*
+Copyright Google LLC. All Rights Reserved.
+Use of this source code is governed by an MIT-style license that
+can be found in the LICENSE file at https://angular.io/license
+*/
---
+++ src/main.ts
@@ -0,0 +1,17 @@
+/*
+*  Protractor support is deprecated in Angular.
+*  Protractor is used in this example for compatibility with Angular documentation tools.
+*/
+import { bootstrapApplication,provideProtractorTestingSupport } from '@angular/platform-browser';
+import { AppComponent } from './app/app.component';
+
+bootstrapApplication(AppComponent,
+    {providers: [provideProtractorTestingSupport()]})
+  .catch(err => console.error(err));
+
+
+/*
+Copyright Google LLC. All Rights Reserved.
+Use of this source code is governed by an MIT-style license that
+can be found in the LICENSE file at https://angular.io/license
+*/
