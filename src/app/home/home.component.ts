5c1e64d6d8b7db54ec7139160e82a7fa04ad3383
morad zakar <morad.azer.1999@gmail.com>
2023-05-16 11:55:06 +0100

    added home and house listing

---
+++ src/app/home/home.component.ts
@@ -0,0 +1,37 @@
+import { Component } from "@angular/core";
+import { CommonModule } from "@angular/common";
+import { HousingLocationComponent } from "../housing-location/housing-location.component";
+import { Housinglocation } from "../housinglocation";
+
+@Component({
+  selector: "app-home",
+  standalone: true,
+  imports: [CommonModule, HousingLocationComponent],
+  template: `
+    <section>
+      <form>
+        <input type="text" placeholder="Filter by city" />
+        <button class="primary" type="button">Search</button>
+      </form>
+    </section>
+    <section class="results">
+      <app-housing-location
+        [housingLocation]="housingLocation"
+        ]
+      ></app-housing-location>
+    </section>
+  `,
+  styleUrls: ["./home.component.css"],
+})
+export class HomeComponent {
+  housingLocation: Housinglocation = {
+    id: 9999,
+    name: "test Home",
+    city: "test City",
+    state: "ST",
+    photo: "assets/example-house.jpg",
+    availableUnits: 99,
+    wifi: true,
+    laundry: false,
+  };
+}
