ef2e43111dd9b61ad02976c1984351145e15540f
morad zakar <morad.azer.1999@gmail.com>
2023-05-17 09:23:19 +0100

    added routing

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,37 +1,56 @@
-import { Component } from "@angular/core";
+import { Component, inject } from "@angular/core";
 import { CommonModule } from "@angular/common";
 import { HousingLocationComponent } from "../housing-location/housing-location.component";
 import { Housinglocation } from "../housinglocation";
+import { HousingService } from "../housing.service";
+import { RouterModule } from "@angular/router";
 
 @Component({
   selector: "app-home",
   standalone: true,
-  imports: [CommonModule, HousingLocationComponent],
+  imports: [CommonModule, HousingLocationComponent, RouterModule],
   template: `
     <section>
       <form>
-        <input type="text" placeholder="Filter by city" />
-        <button class="primary" type="button">Search</button>
+        <input type="text" placeholder="Filter by city" #filtred />
+        <button
+          class="primary"
+          type="button"
+          (click)="filterList(filtred.value)"
+        >
+          Search
+        </button>
       </form>
     </section>
     <section class="results">
       <app-housing-location
+        *ngFor="let housingLocation of tmp"
         [housingLocation]="housingLocation"
-        ]
       ></app-housing-location>
     </section>
   `,
   styleUrls: ["./home.component.css"],
 })
 export class HomeComponent {
-  housingLocation: Housinglocation = {
-    id: 9999,
-    name: "test Home",
-    city: "test City",
-    state: "ST",
-    photo: "assets/example-house.jpg",
-    availableUnits: 99,
-    wifi: true,
-    laundry: false,
-  };
+  housingLocationList: Housinglocation[] = [];
+  housingService: HousingService = inject(HousingService);
+  tmp: Housinglocation[] = [];
+
+  constructor() {
+    this.housingService
+      .getAllHousingLocations()
+      .then((housingLocationList: Housinglocation[]) => {
+        this.housingLocationList = housingLocationList;
+        this.tmp = housingLocationList;
+      });
+  }
+  filterList(value: string) {
+    if (value) {
+      this.tmp = this.tmp.filter((item) =>
+        item?.city.toLowerCase().includes(value.toLowerCase())
+      );
+    } else {
+      this.tmp = this.housingLocationList;
+    }
+  }
 }
---
+++ src/app/housing.service.ts
@@ -0,0 +1,27 @@
+import { Injectable } from "@angular/core";
+import { Housinglocation } from "./housinglocation";
+
+@Injectable({
+  providedIn: "root",
+})
+export class HousingService {
+  constructor() {}
+  url = "http://localhost:3000/locations";
+
+  async getAllHousingLocations(): Promise<Housinglocation[]> {
+    const data = await fetch(this.url);
+    return (await data.json()) ?? [];
+  }
+  async getHousingLocationLocationById(
+    id: number
+  ): Promise<Housinglocation | undefined> {
+    const data = await fetch("${this.url}/${id}");
+
+    return (await data.json()) ?? {};
+  }
+  submitApplication(firstName: string, lastName: string, email: string) {
+    console.log(
+      `Homes application recieved: firstName: ${firstName}, lastName: ${lastName}, email: ${email}.`
+    );
+  }
+}
